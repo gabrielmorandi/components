eb9e742dd06597cd2965a6f7da992a3f7adc4e20
Gabriel Morandi <morandi67766@gmail.com>
2024-01-12 15:24:41 -0300

    feat: adjust styles

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ text }) {
   return (
-    <button className="flex px-4 py-2 text-md font-medium text-white dark:text-zinc-950 transition-colors rounded-md leading-[24px] bg-zinc-900 dark:bg-white hover:bg-zinc-700 dark:hover:bg-zinc-200">
+    <button className="flex px-4 py-2 font-medium leading-5 text-white transition-colors rounded-md text-md dark:text-zinc-950 bg-zinc-900 dark:bg-white hover:bg-zinc-700 dark:hover:bg-zinc-200">
       {text}
     </button>
   )
