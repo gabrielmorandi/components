30ad4b2bd06e021759d75e051ab431d5267b6682
Gabriel Morandi <morandi67766@gmail.com>
2024-01-13 17:35:19 -0300

    feat: accessibility -> outline visible

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ text }) {
   return (
-    <button className="flex px-4 py-2 font-medium leading-5 text-white transition-colors rounded-md text-md dark:text-zinc-950 bg-zinc-900 dark:bg-white hover:bg-zinc-700 dark:hover:bg-zinc-200">
+    <button className="flex px-4 py-2 font-medium leading-5 text-white transition-colors rounded-md text-md outline-offset-2 outline-zinc-900 dark:outline-white dark:text-zinc-950 bg-zinc-900 dark:bg-white hover:bg-zinc-700 dark:hover:bg-zinc-200">
       {text}
     </button>
   )
