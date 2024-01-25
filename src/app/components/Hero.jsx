75dd910dd6d58c43ef8f303badda8b7795799a3a
Gabriel Morandi <morandi67766@gmail.com>
2024-01-25 08:18:47 -0300

    feat: added LinkHero component

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import LinkHero from "./LinkHero"
 export default function Hero() {
   return (
     <section className="flex flex-col w-11/12 place-items-center max-w-[1160px]">
@@ -19,31 +19,25 @@ export default function Hero() {
             <p className="text-lg">
               Should you require any new components or have specific requests,
               feel free to open an issue at{" "}
-              <Link
+              <LinkHero
+                text={"here."}
                 href={"https://github.com/gabrielmorandi/components/issues"}
-                className="relative p-1 rounded-sm bg-transparent hover:text-white z-10 after:content-[''] after:absolute after:w-full after:h-1 after:bg-zinc-900 after:rounded-sm after:left-0 after:bottom-0 after:hover:h-full after:transition-all after:-z-10 focus:after:h-full transition-colors dark:after:bg-white dark:hover:text-zinc-950 dark:focus:text-zinc-950 focus:text-white outline-offset-2"
-              >
-                here.
-              </Link>{" "}
+              />{" "}
               Your contributions and suggestions are highly valued.
             </p>
             <p className="text-lg">
               For further details and to explore the repository, visit{" "}
-              <Link
+              <LinkHero
+                text={"Github repository."}
                 href={"https://github.com/gabrielmorandi/components"}
-                className="relative inline-block w-max p-1 rounded-sm bg-transparent hover:text-white z-10 after:content-[''] after:absolute after:w-full after:h-1 after:bg-zinc-900 after:rounded-sm after:left-0 after:bottom-0 after:hover:h-full after:transition-all after:-z-10 focus:after:h-full transition-colors dark:after:bg-white dark:hover:text-zinc-950 dark:focus:text-zinc-950 focus:text-white outline-offset-2"
-              >
-                Github repository.
-              </Link>
+              />
             </p>
             <p className="text-lg">
               Created by{" "}
-              <Link
+              <LinkHero
+                text={"Gabriel Morandi."}
                 href={"https://github.com/gabrielmorandi/components"}
-                className="inline-block relative p-1 rounded-sm bg-transparent hover:text-white z-10 after:content-[''] after:absolute after:w-full after:h-1 after:bg-zinc-900 after:rounded-sm after:left-0 after:bottom-0 after:hover:h-full after:transition-all after:-z-10 focus:after:h-full transition-colors dark:after:bg-white dark:hover:text-zinc-950 dark:focus:text-zinc-950 focus:text-white outline-offset-2"
-              >
-                Gabriel Morandi.
-              </Link>
+              />
             </p>
           </div>
         </div>
