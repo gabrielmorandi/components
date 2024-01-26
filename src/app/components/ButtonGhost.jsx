5d7079770e4214b2414e5cf39c49f57484a48d75
Gabriel Morandi <morandi67766@gmail.com>
2024-01-26 08:15:30 -0300

    feat: added focus state

--- src/app/components/ButtonGhost.jsx
+++ src/app/components/ButtonGhost.jsx
@@ -1,6 +1,6 @@
 export default function ButtonGhost({ text }) {
   return (
-    <button className="flex px-4 py-2.5 font-medium leading-none transition-colors bg-transparent rounded-md text-md text-zinc-900 dark:text-white hover:bg-zinc-100 dark:hover:bg-zinc-800">
+    <button className="flex px-4 py-2.5 font-medium leading-none transition-colors bg-transparent rounded-md text-md text-zinc-900 dark:text-white hover:bg-zinc-100 focus:bg-zinc-100 dark:hover:bg-zinc-800 dark:focus:bg-zinc-800">
       {text}
     </button>
   )
--- src/app/components/ButtonOutline.jsx
+++ src/app/components/ButtonOutline.jsx
@@ -1,6 +1,6 @@
 export default function ButtonOutline({ text }) {
   return (
-    <button className="flex px-4 py-2.5 text-sm font-medium leading-none transition-colors bg-transparent border rounded-md shadow-sm text-zinc-900 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-800 dark:border-zinc-800">
+    <button className="flex px-4 py-2.5 text-sm font-medium leading-none transition-colors bg-transparent border rounded-md shadow-sm text-zinc-900 hover:bg-zinc-100 focus:bg-zinc-100 dark:text-white dark:hover:bg-zinc-800 dark:focus:bg-zinc-800 dark:border-zinc-800">
       {text}
     </button>
   )
