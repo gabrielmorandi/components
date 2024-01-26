75df13494ba93e5f63eb14d9c5b9275846525001
Gabriel Morandi <morandi67766@gmail.com>
2024-01-26 08:04:01 -0300

    fix: extra width bug and added focus state

--- src/app/components/NewsletterInput.jsx
+++ src/app/components/NewsletterInput.jsx
@@ -9,9 +9,9 @@ export default function NewsletterInput() {
         name="newsletter"
         id="newsletter"
         placeholder="exemple@email.com"
-        className="flex py-2 pl-1 pr-4 bg-transparent placeholder:text-zinc-400 dark:placeholder-zinc-600"
+        className="flex w-full py-2 pl-1 pr-4 bg-transparent placeholder:text-zinc-400 dark:placeholder-zinc-600"
       />
-      <button className="flex px-4 py-2 font-medium text-white transition-colors rounded-full bg-zinc-900 outline-offset-2 dark:bg-white dark:text-zinc-950 hover:bg-zinc-700 dark:hover:bg-zinc-200">
+      <button className="flex px-4 py-2 font-medium text-white transition-colors rounded-full bg-zinc-900 outline-offset-2 dark:bg-white dark:text-zinc-950 hover:bg-zinc-700 focus:bg-zinc-700 dark:hover:bg-zinc-200 dark:focus:bg-zinc-200">
         Subscribe
       </button>
     </label>
